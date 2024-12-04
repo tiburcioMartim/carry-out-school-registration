051da1b18bb08d5e11f6cd08afe7b90cd53dee6f
Martim Tiburcio <tiburciomartim@gmail.com>
2024-12-04 09:51:33 -0300

    update project

--- main.py
+++ main.py
@@ -3,13 +3,22 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait, Select
+import time
 from selenium.webdriver.support import expected_conditions as EC
 from password_generator import gerar_senha, gerar_log # importando a função do gerador
 
 # Lógica do Selenium
 var_service = Service(ChromeDriverManager().install())
 navegator = webdriver.Chrome(service = var_service)
+
+def cadastro_de_responsavel(nome_do_responsavel, email_do_responsavel, senha):
+    navegator.find_element('xpath', '//*[@id="input-15"]').send_keys(nome_do_responsavel)                       # Nome do responsável 
+    navegator.find_element('xpath', '//*[@id="input-18"]').send_keys(email_do_responsavel)                      # E-mail
+    navegator.find_element('xpath', '//*[@id="input-21"]').send_keys(senha)                                     # Gerar uma senha forte
+    navegator.find_element('xpath', '//*[@id="input-26"]').send_keys(senha)                                     # Copiar senha de cima e colar aqui
+    navegator.find_element('xpath', '//*[@id="app"]/div/div[3]/div/div[2]/div/div/div[2]/button').click()       # Clicar em prosseguir pop-up
+    navegator.find_element('xpath', '//*[@id="app"]/div/div[2]/div/div/div/div/div/div[2]/div/button').click()  # Clicar em prosseguir
 
 def dados_aluno(nome_do_aluno, data_nascimento, filiacao_1, 
                 cpf_da_filiacao_1, filiacao_2, CPF_da_filiacao_2, 
@@ -102,8 +111,22 @@ def responsavel(vResponsavel, data_nascimento_responsavel_1, email_do_responsave
         case 'próprio aluno':
             navegator.find_element('xpath', '//*[@id="app"]/div[1]/div[2]/div/div/div/div/div/div/div/div/div[2]/div/div/div[2]/div/div[1]/div[2]/div/div[3]/div/div[2]/button').click() # Aperta próximo
 
-def endereco_telefona():
-    pass
+def endereco_telefone(cep, uf_atual, municipio_atual, bairro_atual, endereço_atual, numero_atual, complemento_atual, ddd_atual_1, telefone_atual_1, ddd_atual_2, telefone_atual_2):
+    navegator.find_element('xpath', '//*[@id="input-462"]').send_keys(cep)
+    navegator.find_element('xpath', '//*[@id="input-467"]').send_keys(uf_atual)
+    navegator.find_element('xpath', '//*[@id="input-467"]').send_keys(Keys.ENTER)
+    navegator.find_element('xpath', '//*[@id="input-473"]').send_keys(municipio_atual)
+    navegator.find_element('xpath', '//*[@id="input-473"]').send_keys(Keys.ENTER)
+    navegator.find_element('xpath', '//*[@id="input-479"]').send_keys(bairro_atual)
+    navegator.find_element('xpath', '//*[@id="input-482"]').send_keys(endereço_atual)
+    navegator.find_element('xpath', '//*[@id="input-485"]').send_keys(numero_atual)
+    navegator.find_element('xpath', '//*[@id="input-488"]').send_keys(complemento_atual)
+    navegator.find_element('xpath', '//*[@id="input-491"]').send_keys(ddd_atual_1)
+    navegator.find_element('xpath', '//*[@id="input-494"]').send_keys(telefone_atual_1)
+    navegator.find_element('xpath', '//*[@id="input-497"]').send_keys(ddd_atual_2)
+    navegator.find_element('xpath', '//*[@id="input-500"]').send_keys(telefone_atual_2)
+    navegator.find_element('xpath', '//*[@id="app"]/div[1]/div[2]/div/div/div/div/div/div/div/div/div[2]/div/div/div[2]/div/div[1]/div[2]/div/div[4]/div/div[2]/button').click() # Aperta próximo
+
 
 # Gerador de senha
 tamanho = 12
@@ -111,8 +134,12 @@ usar_maiusculo = True
 usar_numeros = True
 usar_especiais = True
 
+##########[   CADASTRO DE RESPONSÁVEL   ]##########
+nome_do_responsavel = 'Martim Tiburcio'
+email_do_responsavel = 'tiburcio.contato@gmail.com'
+
 ##########[   DADOS DINÂMICOS   ]##########
-esta_rede_ensino = False                        # implementar vinculo com excel para ser dinâmico
+esta_na_rede_ensino = False                        # implementar vinculo com excel para ser dinâmico
 
 ##########[   DADOS DO ALUNO   ]##########
 nome_do_aluno = 'João Paulo Ferreira'.lower()   # implementar vinculo com excel para ser dinâmico
@@ -140,7 +167,7 @@ uf = 'Rj'.lower()                                         # implementar vinculo 
 data_de_emissao = '22052023'                              # implementar vinculo com excel para ser dinâmico
 
 ##########[   RESPONSÁVEL   ]##########
-vResponsavel = 'próprio aluno'.lower()
+vResponsavel = 'filiação 1'.lower()
 data_nascimento_responsavel_1 = '02101999'      # filiação 1
 email_do_responsavel_1 = 'tiago@gmail.com'      # filiação 1
 data_nascimento_responsavel_2 = '02101999'      # filiação 2
@@ -151,7 +178,22 @@ data_nascimento_responsavel_outro = '02101999'  # responsável outro
 email_do_responsavel_outro = 'tiago@gmail.com'  # responsável outro
 
 ##########[   ENDEREÇO / TELEFONE   ]##########
-# escrever aqui
+cep = '26253789'                    # Campo obrigatório
+uf_atual = 'Rj'                     # Campo obrigatório
+municipio_atual = 'Nova Iguaçu'     # Campo obrigatório
+bairro_atual = 'Posse'              # Campo obrigatório
+endereço_atual = 'Rua do bobo'      # Campo obrigatório
+numero_atual = '0'                  # Campo obrigatório
+complemento_atual = ''
+ddd_atual_1 = '21'                  # Campo obrigatório
+telefone_atual_1 = '988880239'      # Campo obrigatório
+ddd_atual_2 = ''
+telefone_atual_2 = ''
+
+##########[   DADOS DE CADASTRO DA MATRÍCULA   ]##########
+curso = 'EDUCAÇÃO DE JOVENS E ADULTOS - EJA'  # Campo obrigatório
+serie = 'FASE II'                             # Campo obrigatório
+possui_irmao_na_escola = 'N'.lower()          # Campo obrigatório
 
 senha = gerar_senha(tamanho, usar_maiusculo, usar_numeros, usar_especiais)
 gerar_log(tamanho, usar_maiusculo, usar_numeros, usar_especiais, senha)
@@ -159,43 +201,51 @@ gerar_log(tamanho, usar_maiusculo, usar_numeros, usar_especiais, senha)
 try:
     navegator.get('https://matricula.semed.novaiguacu.rj.gov.br/#/inscricaoUsuario')
     
-    navegator.find_element('xpath', '//*[@id="input-15"]').send_keys('Martim Tiburcio')                         # Nome do responsável 
-    navegator.find_element('xpath', '//*[@id="input-18"]').send_keys('tiburcio.contato@gmail.com')              # E-mail
-    navegator.find_element('xpath', '//*[@id="input-21"]').send_keys(senha)                                     # Gerar uma senha forte
-    navegator.find_element('xpath', '//*[@id="input-26"]').send_keys(senha)                                     # Copiar senha de cima e colar aqui
-    navegator.find_element('xpath', '//*[@id="app"]/div/div[3]/div/div[2]/div/div/div[2]/button').click()       # Clicar em prosseguir pop-up
-    navegator.find_element('xpath', '//*[@id="app"]/div/div[2]/div/div/div/div/div/div[2]/div/button').click()  # Clicar em prosseguir
+    cadastro_de_responsavel(nome_do_responsavel, email_do_responsavel, senha)
     WebDriverWait(navegator, 1).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app"]/div/div[2]/div/div/div/div/div[2]/div/span/div/span/div/div/div/div/div[1]/div/div/div/div/div'))) # Aguarda o botão carregar
-
-    if esta_rede_ensino:    # Faz parte do sistema de ensino? SIM SIM SIM
+    if esta_na_rede_ensino:    # Faz parte do sistema de ensino? SIM SIM SIM
         navegator.find_element('xpath', '//*[@id="app"]/div/div[2]/div/div/div/div/div[2]/div/span/div/span/div/div/div/div/div[1]/div/div/div/div/div').click() # Aperta sim
         navegator.find_element('xpath', '//*[@id="app"]/div/div[2]/div/div/div/div/div[2]/div/span/div[2]/span/div/div/div/div/div[3]/div/div/div/div/div').click() # Clica em Nome
         navegator.find_element('xpath', '//*[@id="input-104"]').send_keys('João Paulo Ferreira')        # Preenche nome do aluno
         navegator.find_element('xpath', '//*[@id="input-107"]').send_keys('Carolina Da Silva Erreira')  # Preenche nome da filiação 1
         navegator.find_element('xpath', '//*[@id="input-101"]').send_keys('12022011')                   # Preenche data de nascimento
         # navegator.find_element('xpath', '//*[@id="recaptcha-anchor"]/div[1]').click() # Clica no eu não sou um robô   ********** QUEBRAR CAPTCHA **********
-        input('Para encerrar pressione qualquer tecla.')
+        time.sleep(1000)
         
-    else:   # Faz parte do sistema de ensino? NÃO NÃO NÃO
+    else: ######## Faz parte do sistema de ensino? NÃO NÃO NÃO
         navegator.find_element('xpath', '//*[@id="app"]/div/div[2]/div/div/div/div/div[2]/div/span/div/span/div/div/div/div/div[2]/div/div/div/div/div').click() # Aperta não
         navegator.find_element('xpath', '//*[@id="app"]/div/div[2]/div/div/div/div/div[2]/div/button').click()  # Aperta prosseguir
         
-        dados_aluno(nome_do_aluno, data_nascimento, filiacao_1, 
-                    cpf_da_filiacao_1, filiacao_2, CPF_da_filiacao_2, 
-                    email_do_aluno, cor, nacionalidade, uf_nascimento, 
-                    municipio_nascimento, fator_rh, procedencia_escola)        
-        
+        dados_aluno(nome_do_aluno, data_nascimento, filiacao_1, cpf_da_filiacao_1, filiacao_2, CPF_da_filiacao_2, email_do_aluno, cor, nacionalidade, uf_nascimento, municipio_nascimento, fator_rh, procedencia_escola)        
         documentos(cpf_do_aluno, nis, rg, rg_digito, orgao_expedidor, uf, data_de_emissao)
+        responsavel(vResponsavel, data_nascimento_responsavel_1, email_do_responsavel_1, data_nascimento_responsavel_2, email_do_responsavel_2, cpf_responsavel_outro, nome_do_responsavel_outro, data_nascimento_responsavel_outro, email_do_responsavel_outro)
+        endereco_telefone(cep, uf_atual, municipio_atual, bairro_atual, endereço_atual, numero_atual, complemento_atual, ddd_atual_1, telefone_atual_1, ddd_atual_2, telefone_atual_2)
         
-        responsavel(vResponsavel, data_nascimento_responsavel_1, email_do_responsavel_1, 
-                data_nascimento_responsavel_2, email_do_responsavel_2, cpf_responsavel_outro, 
-                nome_do_responsavel_outro, data_nascimento_responsavel_outro, email_do_responsavel_outro)
+        ######################     DADOS DE CADASTRO DA MATRÍCULA
         
-        
-        
-        
-        
-    # navegator.find_element('xpath', '')
+        navegator.find_element('xpath', '//*[@id="input-585"]').send_keys(curso)
+        navegator.find_element('xpath', '//*[@id="input-585"]').send_keys(Keys.ENTER)
+        navegator.find_element(By.XPATH, '//*[@id="input-591"]').send_keys(serie)           
+        navegator.find_element(By.XPATH, '//*[@id="input-591"]').send_keys(Keys.ARROW_DOWN) 
+        navegator.find_element(By.XPATH, '//*[@id="input-591"]').send_keys(Keys.ENTER)
+        navegator.find_element('xpath', '//*[@id="app"]/div[1]/div[2]/div/div/div/div/div/div/div/div/div[3]/div/div/div[3]/div[2]/div/span/div[3]/div[1]/div[2]/button').click() # PESQUISAR ESCOLA
 
+        ########### Escolher a escola em uma variedade de escolas ###########
+        
+        
+        time.sleep(1000)
+        if possui_irmao_na_escola == 's': # SIM
+            navegator.find_element('xpath', '//*[@id="app"]/div[1]/div[2]/div/div/div/div/div/div/div/div/div[3]/div/div/div[3]/div[2]/div/span/div[5]/span/div/div/div/div/div[1]/div/div/div/div/div').click() # SIM
+        elif possui_irmao_na_escola == 'n': # NÃO
+            navegator.find_element('xpath', '//*[@id="app"]/div[1]/div[2]/div/div/div/div/div/div/div/div/div[3]/div/div/div[3]/div[2]/div/span/div[5]/span/div/div/div/div/div[2]/div/div/div/div/div').click() # NÃO
+            time.sleep(1000)
+        
+        
+        
+        
+        # navegator.find_element('xpath', '')
+    
+    time.sleep(1000)
+    
 except TypeError as e:
     print(f'Erro: {e}')
